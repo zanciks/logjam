8b4e9e6120fad02f6c91c0ff1d189824f9952ae0
Zanciks <Zackarysemancik@gmail.com>
2024-08-16 19:37:18 +0000

    comment on why it doesn't work

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+// technically this won't run right now. We need to move patterns.xml, index.html, and script.js to a plugin 
+// and then implement plugin loading. It is expected to be broken right now!!
+
 use tauri::{generate_handler, generate_context};
 mod log_parser;
 
