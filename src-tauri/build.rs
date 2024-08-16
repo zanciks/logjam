b888c52a3cb46b4f3664e556659030b7b45c76fe
Zanciks <Zackarysemancik@gmail.com>
2024-08-16 21:18:36 +0000

    working on build script for portable target

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
 fn main() {
     tauri_build::build();
+
+    let profile = env::var("PROFILE").unwrap();
+    if let Ok(profile) = env::var("PROFILE") {
+        if profile == "debug" {
+            let build_path = Path::new(".").join("target").join(profile).join("logjam");
+            let target_path = Path::new(".").join("portable");
+            fs::copy(build_path, target_path).expect("Could not copy executable");
+        }
+    }
 }
