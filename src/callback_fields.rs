e75a1a93a5c7f8d7afbe294a8f6e4a11ff198f5b
Zanciks <Zackarysemancik@gmail.com>
2024-09-13 21:31:53 +0000

    callback fields

--- src/callback_fields.rs
+++ src/callback_fields.rs
@@ -3,26 +3,46 @@ use std::fs;
 use roxmltree::Document;
 
 pub struct CallbackField {
-    element_name: String,
-    pattern: String,
-    file_name: String,
+    pub name: String,
+    pub pattern: String,
+    pub file_name: String,
 }
 
-// todo
-// function Self::list(path: Path)
-// if path contains a callbackFields.xml file, then
-// // parse the XML and return a vector of Self 
-// // based on what is in the file. 
-// return the Vec<CallbackField>
+impl Default for CallbackField {
+    fn default() -> Self {
+        CallbackField {
+            name: String::new(),
+            pattern: String::new(),
+            file_name: String::new()
+        }
+    }
+}
 
 pub fn list_all(plugin_path: &Path) -> Vec<CallbackField> {
-    let callback_fields: Vec<CallbackField> = vec![];
+    let mut callback_fields: Vec<CallbackField> = vec![];
 
     if plugin_path.join("callbackFields.xml").exists() {
         let xml = fs::read_to_string(&plugin_path.join("callbackFields.xml")).unwrap();
         let document = Document::parse(&xml).unwrap();
         for node in document.descendants() {
-            if node.has_tag_name("element") { println!("test!") }
+            if node.has_tag_name("callbackField") {
+                let mut callback_field = CallbackField::default();
+
+                for child in node.descendants() {
+                    match child.tag_name().name() {
+                        "name"     => { callback_field.name = child.tag_name().name().to_string() },
+                        "pattern"  => { callback_field.pattern      = child.tag_name().name().to_string() },
+                        "fileName" => { callback_field.file_name    = child.tag_name().name().to_string() },
+                        _ => ()
+                    }
+                }
+
+                if callback_field.name != String::new() 
+                    && callback_field.pattern != String::new() 
+                    && callback_field.file_name != String::new() {
+                        callback_fields.push(callback_field);
+                    }
+            }
         }
     }
 
