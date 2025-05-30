ed9aed84908e3e2d5a870851b943a27ea21ec45d
zanciks <zackarysemancik@gmail.com>
2025-05-30 12:40:31 -0600

    fixed responses

--- logjam_core/src/response.rs
+++ logjam_core/src/response.rs
@@ -2,112 +2,90 @@ use eframe::egui;
 
 #[repr(C)]
 pub struct Response {
-    response: *const egui::Response,
+    response: egui::Response,
 }
 
 impl From<egui::Response> for Response {
-    fn from(from: egui::Response) -> Self {
+    fn from(response: egui::Response) -> Self {
         Self {
-            response: &from as *const egui::Response,
+            response,
         }
     }
 }
 
 impl Response {
     pub fn clicked(&self) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.clicked()
+        self.response.clicked()
     }
     /*
     pub fn clicked_by(&self, button: PointerButton) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.clicked_by(button)
+        self.response.clicked_by(button)
     }
     */
     pub fn secondary_clicked(&self) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.secondary_clicked()
+        self.response.secondary_clicked()
     }
     pub fn long_touched(&self) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.long_touched()
+        self.response.long_touched()
     }
     pub fn middle_clicked(&self) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.middle_clicked()
+        self.response.middle_clicked()
     }
     pub fn double_clicked(&self) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.double_clicked()
+        self.response.double_clicked()
     }
     pub fn triple_clicked(&self) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.triple_clicked()
+        self.response.triple_clicked()
     }
     /*
     pub fn double_clicked_by(&self, button: PointerButton) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.double_clicked_by(button)
+        self.response.double_clicked_by(button)
     }
     pub fn triple_clicked_by(&self, button: PointerButton) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.triple_clicked_by(button)
+        self.response.triple_clicked_by(button)
     }
     */
     pub fn enabled(&self) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.enabled()
+        self.response.enabled()
     }
     pub fn hovered(&self) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.hovered()
+        self.response.hovered()
     }
     pub fn contains_pointer(&self) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.contains_pointer()
+        self.response.contains_pointer()
     }
     pub fn has_focus(&self) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.has_focus()
+        self.response.has_focus()
     }
     pub fn gained_focus(&self) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.gained_focus()
+        self.response.gained_focus()
     }
     pub fn lost_focus(&self) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.lost_focus()
+        self.response.lost_focus()
     }
     pub fn request_focus(&self) {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.request_focus();
+        self.response.request_focus();
     }
     pub fn surrender_focus(&self) {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.surrender_focus();
+        self.response.surrender_focus();
     }
     pub fn drag_started(&self) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.drag_started()
+        self.response.drag_started()
     }
     /*
     pub fn drag_started_by(&self, button: PointerButton) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.drag_started_by(button)
+        self.response.drag_started_by(button)
     }
     */
     pub fn dragged(&self) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.dragged()
+        self.response.dragged()
     }
     /*
     pub fn dragged_by(&self, button: PointerButton) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.dragged_by(button)
+        self.response.dragged_by(button)
     }
     */
     pub fn drag_stopped(&self) -> bool {
-        let response = unsafe { self.response.as_ref().unwrap() };
-        response.drag_stopped()
+        self.response.drag_stopped()
     }
 }
