266856c07c041723ed3c2332ecadb402e28068f0
zanciks <zackarysemancik@gmail.com>
2025-05-30 12:33:20 -0600

    testing

--- template_plugin/src/lib.rs
+++ template_plugin/src/lib.rs
@@ -11,7 +11,7 @@ impl LogjamPlugin for TemplatePlugin {
     fn render(&mut self, ui: &UiWrapper) {
         ui.label("This is a label from a dll!");
         if ui.button("dll button~").clicked() {
-            println!("button was clicked in a dll!!");
+            ui.label("one frame test label");
         }
     }
 }
