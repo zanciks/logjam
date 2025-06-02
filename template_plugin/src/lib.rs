e5bce5bd18b81fe67d5e9cb8fcbb4a1168da6957
zanciks <zackarysemancik@gmail.com>
2025-06-02 16:07:57 -0600

    Updated template plugin

--- template_plugin/src/lib.rs
+++ template_plugin/src/lib.rs
@@ -3,16 +3,19 @@ use logjam_core::{LogjamPlugin, plugin::LogjamPlugin, ui::UiWrapper};
 use eframe::egui::Ui;
 
 #[derive(Default, LogjamPlugin)]
-struct TemplatePlugin;
+struct TemplatePlugin {
+	count: u32,
+}
 
 impl LogjamPlugin for TemplatePlugin {
     fn title(&self) -> RString {
         RString::from("Template Plugin")
     }
     fn render(&mut self, ui: &mut Ui) {
+		ui.label(format!("click count: {}", self.count));
         ui.label("This is a label from a dll!");
         if ui.button("dll button~").clicked() {
-            println!("Button clicked!");
+            self.count += 1;
         }
     }
 }
